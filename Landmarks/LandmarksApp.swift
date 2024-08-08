dbc34b07e46f6d59b4417f946aae79e6d79a1bae
Diego Martinez <diegogama8934@gmail.com>
2024-08-07 19:42:06 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Diego Martínez García on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
